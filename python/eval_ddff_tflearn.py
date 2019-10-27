4634593eaaf75018b9b30c6ef264f13222ed51b9
gritYCDA <gritYCDA@gmail.com>
2019-10-28 01:29:52 +0900

    ddff baseline and add interframe layer

---
+++ python/eval_ddff_tflearn.py
@@ -0,0 +1,19 @@
+import ddff.dataproviders.datareaders.FocalStackDDFFH5Reader as FocalStackDDFFH5Reader
+import ddff.metricseval.DDFFTFLearnEval as DDFFTFLearnEval
+
+if __name__ == "__main__":
+    #Set parameters
+    image_size = (383,552)
+    filename_testset = "../../ddff-dataset-trainval.h5"
+    checkpoint_file = "ddffnet-cc3-snapshot-121256.npz"
+    stack_key = "stack_val"
+    disp_key="disp_val"
+
+    #Create validation reader
+    tmp_datareader = FocalStackDDFFH5Reader.FocalStackDDFFH5Reader(filename_testset, transform=None, stack_key=stack_key, disp_key=disp_key)
+
+    #Create PSPDDFF evaluator
+    evaluator = DDFFTFLearnEval.DDFFTFLearnEval(checkpoint_file, focal_stack_size=tmp_datareader.get_stack_size(), norm_mean=None, norm_std=None)
+    #Evaluate
+    metrics = evaluator.evaluate(filename_testset, stack_key=stack_key, disp_key=disp_key, image_size=image_size)
+    print(metrics)
---
+++ python/run_ddff.py
@@ -0,0 +1,42 @@
+#! /usr/bin/python3
+
+import argparse
+import torch
+import random
+import numpy as np
+import ddff.trainers.DDFFTrainer as DDFFTrainer
+
+if __name__ == "__main__":
+    #Add command line parser arguments
+    parser = argparse.ArgumentParser(description='Train ddff net on specified h5 dataset.')
+    parser.add_argument('--dataset', default="../../ddff-dataset-trainval.h5", help='h5 file that contains the training and validation data (default: ddff-dataset-trainval.h5)')
+    parser.add_argument('--epochs', default=2, type=int, help='number of training epochs (default: 200)')
+    parser.add_argument('--checkpoint', default="ddff_cc3_checkpoint.pt", help='Checkpoint file to be created during training (default: ddff_cc3_checkpoint.pt)')
+    parser.add_argument('--checkpoint_frequency', default=2, type=int, help='Checkpoint frequency to save intermediate models. (default: 5)')
+    parser.add_argument('--workers', default=0, type=int, help='Number of threads reading the dataset. (default: 0)')
+    parser.add_argument('--batchsize', default=2, type=int, help='batch size during training (default: 2)')
+    parser.add_argument('--pretrained', default="bn", help='Either specify a npy file to load tensorflow weights or use "bn" or "no_bn" to use pretrained weights from torchvision package (default: bn)')
+
+    #Parse arguments
+    args = parser.parse_args()
+
+    #Finetune tensorflow vgg16 model
+    ddff_trainer = DDFFTrainer.DDFFTrainer.from_h5_data(args.dataset,
+                    learning_rate=0.001,
+                    max_gradient=5.0,
+                    inter_frame_learn=False,
+                    cc1_enabled=False,
+                    cc2_enabled=False,
+                    cc3_enabled=True,
+                    cc4_enabled=False,
+                    cc5_enabled=False,
+                    training_crop_size=None,
+                    validation_crop_size=None,
+                    pretrained=args.pretrained,
+                    normalize_mean=[0.485, 0.456, 0.406], normalize_std=[0.229, 0.224, 0.225],
+                    epochs=args.epochs,
+                    checkpoint_file=args.checkpoint,
+                    checkpoint_frequency=args.checkpoint_frequency,
+                    batch_size=args.batchsize,
+                    num_workers=args.workers,
+                    deterministic=True)
